b9f9ab4494bd93af517430a7cca8a00044260e8f
Yago González <yagogonzalezg@gmail.com>
2016-01-01 17:30:27 +0100

    Implement #2

--- Browser Geolocation/script.js
+++ Browser Geolocation/script.js
@@ -5,9 +5,17 @@ function geolocSuccess(position) {
 	errorAlert.style.display = 'none';	// Hide the error box	
 
 	resultBox.style.display = 'block';	// Display the result box
+	
+	var latDeg = Math.floor(position.coords.latitude);
+	var latMin = Math.floor(position.coords.latitude % 1 * 60);
+	var latSec = position.coords.latitude % 1 * 60 % 1;
 
-	document.getElementById('latitude').innerHTML = position.coords.latitude;
-	document.getElementById('longitude').innerHTML = position.coords.longitude;
+	var lonDeg = Math.floor(position.coords.longitude);
+	var lonMin = Math.floor(position.coords.longitude % 1 * 60);
+	var lonSec = position.coords.longitude % 1 * 60 % 1;
+
+	document.getElementById('latitude').innerHTML = position.coords.latitude + ' (' + latDeg + 'º ' + latMin + '\' ' + latSec + '")';
+	document.getElementById('longitude').innerHTML = position.coords.longitude + ' (' + lonDeg + 'º ' + lonMin + '\' ' + lonSec + '")';
 }
 
 function geolocError(err) {	// Something went wrong when asking for the position
